561388c0cb0f1bcca6a56e77eb3ca39b6eb6f7ff
Ryan Leach <rleach@Ryans-MacBook-Air.local>
2019-06-12 09:19:32 -0700

    Add CopyOverlay animation

--- color-palette-app2/src/components/ColorBox.tsx
+++ color-palette-app2/src/components/ColorBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import CopyToClipboard from "react-copy-to-clipboard";
 
@@ -13,6 +13,16 @@ const StyledBox = styled("div")<{ background: string }>`
   text-transform: uppercase;
 `;
 
+const H1 = styled("h1")`
+  font-weight: 400;
+  text-shadow: 1px 2px black;
+  background: rgba(255, 255, 255, 0.2);
+  width: 100%;
+  text-align: center;
+  margin-bottom: 0;
+  padding: 1rem;
+`;
+
 const CopyContainer = styled("div")``;
 
 const BoxContent = styled("div")`
@@ -24,6 +34,51 @@ const BoxContent = styled("div")`
   color: black;
   font-size: 12px;
   letter-spacing: 1px;
+`;
+
+const CopyOverlay = styled("div")<{ active: boolean; background: string }>`
+  opacity: 0;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  transform: scale(0.1);
+  background: ${props => props.background};
+  transition: transform 0.3s ease-in-out;
+
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1;
+    transform: scale(9);
+    z-index: 10;
+    position: absolute;
+    `}
+`;
+
+const CopyOverlayMessage = styled("div")<{ active: boolean }>`
+  opacity: 0;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 2rem;
+  transform: scale(0.1);
+  color: white;
+
+  ${({ active }) =>
+    active &&
+    `
+  opacity: 1;
+  transform: scale(1);
+  z-index: 15;
+  transition: all 0.4s ease-in-out;
+  transition-delay: 0.3s;
+  `}
 `;
 
 const CopyButton = styled("button")`
@@ -67,21 +122,44 @@ type Props = {
   name: string;
 };
 
-const ColorBox: React.FC<Props> = props => {
-  const { background, name } = props;
-  return (
-    <StyledBox background={background}>
-      <CopyContainer>
-        <BoxContent>
-          <span>{name}</span>
-        </BoxContent>
-        <CopyToClipboard text={background}>
-          <CopyButton>Copy</CopyButton>
-        </CopyToClipboard>
-      </CopyContainer>
-      <MoreButton>More</MoreButton>
-    </StyledBox>
-  );
+type State = {
+  active: boolean;
 };
+
+class ColorBox extends Component<Props, State> {
+  state: State = {
+    active: false
+  };
+
+  changeCopyState = () => {
+    this.setState({ active: true }, () => {
+      setTimeout(() => this.setState({ active: false }), 1500);
+    });
+  };
+
+  render() {
+    const { background, name } = this.props;
+    return (
+      <StyledBox background={background}>
+        <CopyOverlay background={background} active={this.state.active} />
+        <CopyOverlayMessage active={this.state.active}>
+          <H1>Copied!</H1>
+          <p style={{ fontWeight: 200, fontSize: "1.5rem" }}>
+            {this.props.background}
+          </p>
+        </CopyOverlayMessage>
+        <CopyContainer>
+          <BoxContent>
+            <span>{name}</span>
+          </BoxContent>
+          <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+            <CopyButton>Copy</CopyButton>
+          </CopyToClipboard>
+        </CopyContainer>
+        <MoreButton>More</MoreButton>
+      </StyledBox>
+    );
+  }
+}
 
 export default ColorBox;
