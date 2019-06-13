e56600105c28279d7989b441d55f47e732fb7095
Ryan Leach <rleach@Ryans-MacBook-Air.local>
2019-06-12 20:46:38 -0700

    Add colorHelper to generate colors

--- color-palette-app2/src/App.tsx
+++ color-palette-app2/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Palette from "./components/Palette";
 import seedPalettes from "./components/seedPalettes";
+import { generatePalette } from "./components/colorHelpers";
 
 const App: React.FC = () => {
+  console.log(generatePalette(seedPalettes[4]));
   return (
     <div className="App">
       <Palette {...seedPalettes[3]} />
---
+++ color-palette-app2/src/components/colorHelpers.ts
@@ -0,0 +1,54 @@
+import chroma from "chroma-js";
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+interface paletteOptions {
+  paletteName: string;
+  id: string;
+  emoji: string;
+  colors: any;
+}
+
+function generatePalette(starterPalette: paletteOptions) {
+  let newPalette: any = {
+    paletteName: starterPalette.paletteName,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
+    colors: {}
+  };
+  for (let level of levels) {
+    newPalette.colors[level] = [];
+  }
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, 10).reverse();
+    for (let i in scale) {
+      newPalette.colors[levels[i]].push({
+        name: `${color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
+        rgb: chroma(scale[i]).css(),
+        hsl: chroma(scale[i]).css("hsl"),
+        rgba: chroma(scale[i]).css("rgba")
+      });
+    }
+  }
+  return newPalette;
+}
+
+function getRange(hexColor: string) {
+  return [
+    chroma(hexColor)
+      .darken()
+      .hex(),
+    hexColor,
+    "#fff"
+  ];
+}
+
+function getScale(hexColor: string, numberOfColors: number) {
+  return chroma
+    .scale(getRange(hexColor))
+    .mode("lab")
+    .colors(numberOfColors);
+}
+
+export { generatePalette };
