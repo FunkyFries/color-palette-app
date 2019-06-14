c1320ff75ca6d84687519a9053ae60310e9356b6
Ryan Leach <leachrp@comcast.net>
2019-06-14 07:43:51 -0700

    Add color Slider

--- color-palette-app2/src/App.tsx
+++ color-palette-app2/src/App.tsx
@@ -4,10 +4,9 @@ import seedPalettes from "./components/seedPalettes";
 import { generatePalette } from "./components/colorHelpers";
 
 const App: React.FC = () => {
-  console.log(generatePalette(seedPalettes[4]));
   return (
     <div className="App">
-      <Palette {...seedPalettes[3]} />
+      <Palette palette={generatePalette(seedPalettes[4])} />
     </div>
   );
 };
--- color-palette-app2/src/components/Palette.tsx
+++ color-palette-app2/src/components/Palette.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import ColorBox from "./ColorBox";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 const PaletteDiv = styled("div")`
   height: 100vh;
@@ -11,17 +13,40 @@ const ColorBoxes = styled("div")`
   flex-wrap: wrap;
 `;
 
+interface Palette {
+  paletteName: string;
+  id: string;
+  emoji: string;
+  colors: { [num: number]: Array<Object> };
+}
+
 type Props = {
-  colors: Array<{ name: string; color: string }>;
+  palette: Palette;
 };
 
 class Palette extends Component<Props> {
+  state = {
+    level: 500
+  };
+
+  handleChange = (level: number) => {
+    this.setState({ level });
+  };
   render() {
-    const colorBoxes = this.props.colors.map(color => (
-      <ColorBox key={color.name} background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color: any) => (
+      <ColorBox key={color.name} background={color.hex} name={color.name} />
     ));
     return (
       <PaletteDiv>
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.handleChange}
+        />
         <ColorBoxes>{colorBoxes}</ColorBoxes>
       </PaletteDiv>
     );
