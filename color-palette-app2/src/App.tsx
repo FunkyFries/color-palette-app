51eb254a0a31e60affc5adfcc5ab8be740ff44c4
Ryan Leach <leachrp@comcast.net>
2019-07-12 07:32:09 -0700

    Add basic route transitions

--- color-palette-app2/src/App.tsx
+++ color-palette-app2/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Router, Redirect } from "@reach/router";
+import { Router, Redirect, Location } from "@reach/router";
 import seedPalettes from "./components/Helpers/seedPalettes";
 import Home from "./components/Home/index";
 import { NewPalette } from "./components/NewPalette/index";
 import Palette from "./components/Palette/index";
 import SingleColorPalette from "./components/SingleColorPalette/index";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const savedPalettes = localStorage.getItem("palettes");
 
@@ -32,24 +33,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <Home
-            path="/"
-            palettes={this.state.palettes}
-            deletePalette={this.deletePalette}
-          />
-          <NewPalette
-            path="/palette/new"
-            savePalette={this.savePalette}
-            palettes={this.state.palettes}
-          />
-          <Palette path="/palette/:id" palettes={this.state.palettes} />
-          <SingleColorPalette
-            path="/palette/:paletteId/:colorId"
-            palettes={this.state.palettes}
-          />
-          <Redirect default noThrow from="*" to="/" />
-        </Router>
+        <Location>
+          {({ location }) => (
+            <TransitionGroup className="transition-group">
+              <CSSTransition key={location.key} classNames="fade" timeout={500}>
+                <Router location={location} className="router">
+                  <Home
+                    path="/"
+                    palettes={this.state.palettes}
+                    deletePalette={this.deletePalette}
+                  />
+                  <NewPalette
+                    path="/palette/new"
+                    savePalette={this.savePalette}
+                    palettes={this.state.palettes}
+                  />
+                  <Palette path="/palette/:id" palettes={this.state.palettes} />
+                  <SingleColorPalette
+                    path="/palette/:paletteId/:colorId"
+                    palettes={this.state.palettes}
+                  />
+                  <Redirect default noThrow from="*" to="/" />
+                </Router>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        </Location>
       </div>
     );
   }
